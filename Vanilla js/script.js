38f4e249f9dae18c6b5cc543f3488906338c08f6
Jafar610 <jafarmohamed955@gmail.com>
2026-02-25 22:51:17 +0300

    file updated

--- Vanilla js/script.js
+++ Vanilla js/script.js
@@ -2,8 +2,9 @@ let fruit = ['banana', 'apple', 'grape'];
 let x = 0;
 setInterval(() => {
     fruit[0] = x % 2 === 0 ? 'banana' : 'apple';
-    let cssClass = fruit[0] === 'banana' ? 'yellow' : 'red';
-    document.getElementById('list').innerHTML = `<ul><li class="${cssClass}">${fruit[0]}</li><li>${fruit[1]}</li><li>${fruit[2]}</li></ul>`;
+    let cssClass = fruit[0] === 'banana' ? 'banana' : 'apple';
+    document.getElementById('list').innerHTML = `
+    <ul><li class="${cssClass}">${fruit[0]}</li><li>${fruit[1]}</li><li>${fruit[2]}</li></ul>`;
     x++;
 }, 1000);
 
